d910fbbcce1bf818b112ff962e61023060268f11
Murad Malik <muradmalik7@gmail.com>
2019-04-01 22:35:26 +0500

    project initialized with router view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import router from "./router";
+import Vuetify from 'vuetify'
+import "./assets/scss/theme.scss"
+
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+var vueInstance = new Vue({
+  el: "#app",
+  template: "<App/>",
+  router,
+  render: h => h(App)
+});
+
+export default vueInstance;
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <v-layout>
+            <h2>shaheer hashim</h2>
+        </v-layout>
+    </v-container>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+
+</style>
